f40d0246210b42412fca61be9fb3bb43498c9308
jonathan-cederquist <97691358+jonathan-cederquist@users.noreply.github.com>
2022-01-26 18:13:14 -0800

    Update EncoderDriver.py

--- src/EncoderDriver.py
+++ src/EncoderDriver.py
@@ -1,3 +1,13 @@
+''' @file       EncoderDriver.py
+    @brief      Used to read from encoders attached to motors
+    @details    Utilizes alternate functions from timer channels on pyb board to 
+                read from attached encoders
+    @author     Jonathan Cederquist
+    @author     Tim Jain
+    @author     Philip Pang
+    @date       Last Modified 1/26/22
+'''
+
 import pyb
 import utime
 
@@ -5,52 +15,64 @@ class EncoderDriver:
     '''! 
     This class implements an encoder driver for an ME405 kit. 
     '''
-    current_position = 0
-    delta = 0
     
     def __init__ (self, in1pin, in2pin, timer):
         '''! 
-        Creates an encoder driver by initializing GPIO
-        pins and turning the motor off for safety. 
-        @param in1pin
-        @param in2pin
-        @param timer
+        @brief          Creates an encoder driver object
+        @details        Creates an encoder driver by initializing timers and channels with 
+                        given pins and timer number
+        @param in1pin   A pyb.Pin object corresponding to the encoder channel A 
+        @param in2pin   A pyb.Pin object corresponding to the encoder channel B
+        @param timer    The timer number corresponding to the encoder pins
         '''
+        
+        # Create timer and timer channels in encoder mode
         self.timer = pyb.Timer(timer, prescaler = 0, period = 65535)
-        ch1 = self.timer.channel(1, pyb.Timer.ENC_AB, pin = in1pin)
-        ch2 = self.timer.channel(2, pyb.Timer.ENC_AB, pin = in2pin)
+        self.ch1 = self.timer.channel(1, pyb.Timer.ENC_AB, pin = in1pin)
+        self.ch2 = self.timer.channel(2, pyb.Timer.ENC_AB, pin = in2pin)
+        
+        # Stores current encoder position in radians
+        self.current_position = 0
+        self.delta = 0
         
 
     def update(self):
-
-        
-        '''@brief       This updates the position of the encoder based on the
-                        timer count and previous position. 
-                        needs at least 2 values in each period
-           @details     if >=2 values then delta can be accurately recorded
-                        saved and then subtracted from last known value '''
+        '''!
+        @brief      Updates the position of the encoder 
+        @details    Updates the position of the encoder using saved last value
+                    Checks for a 'valid' delta and adjusts if needed, then adds
+                    delta to previous position 
+        '''
 
         prev_position = self.current_position % 65535
         self.delta = self.timer.counter() - prev_position
+        
+        # Validate and adjust delta
         if self.delta < -65535/2:
             self.delta += 65535
         elif self.delta > 65535/2:
             self.delta -= 65535
+        
+        # Update position
         self.current_position += self.delta
     
     def read (self):
         '''!
-        @brief Returns position of encoder
-        @details This multiplies the current position reading (in ticks) by pi and divides it by the
-        CPR specs of the encoder times the gear ratio in order to convert to radians
+        @brief      Returns current position of encoder
+        @details    Converts the current encoder position reading (in ticks)
+                    to radians using the encoder CPR and gear ratio
         '''
-        return self.current_position * 3.1415926535 / 8192 #256*4*8
+        gearRatio = 16
+        CPR = 256
+        
+        return self.current_position * 3.1415926535 / (gearRatio*CPR*2)
     
 
     def zero(self):
         '''!
-        @brief This method zeros out the encoder
+        @brief      Zeros the encoder position
         '''
+        
         self.current_position = 0
         self.delta = 0
         
