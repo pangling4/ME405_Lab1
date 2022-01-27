4010038d786cc207da5bf89422e18dbe384baef5
tjain1234 <jaint.17.mhs@gmail.com>
2022-01-26 16:27:13 -0800

    Documentation Test Lab 1
    Testing to see doxygen interfacing with Github Pages

---
+++ docs/annotated_dup.js
@@ -0,0 +1,9 @@
+var annotated_dup =
+[
+    [ "EncoderDriver", null, [
+      [ "EncoderDriver", "class_encoder_driver_1_1_encoder_driver.html", "class_encoder_driver_1_1_encoder_driver" ]
+    ] ],
+    [ "MotorDriver", null, [
+      [ "MotorDriver", "class_motor_driver_1_1_motor_driver.html", "class_motor_driver_1_1_motor_driver" ]
+    ] ]
+];
---
+++ docs/class_encoder_driver_1_1_encoder_driver.js
@@ -0,0 +1,5 @@
+var class_encoder_driver_1_1_encoder_driver =
+[
+    [ "__init__", "class_encoder_driver_1_1_encoder_driver.html#aa100dd5b48ac3acc3da5679edfd95239", null ],
+    [ "update", "class_encoder_driver_1_1_encoder_driver.html#a801d099176eaeb5ae628fef95f978680", null ]
+];
